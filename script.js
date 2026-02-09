5bdced5224848beb5c2d8db973f379526ad6a6d4
cristhianbareiro922-cell <cristhianbareiro922@gmail.com>
2026-02-09 15:27:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const pieces = document.querySelectorAll('.piece');
 pieces.forEach((piece, index) => {
   setTimeout(() => {
     piece.classList.add('show');
-  }, index * 700);
+  }, index * 1000);
 });
